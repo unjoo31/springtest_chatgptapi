88fddc06495256f3b62f23ff1631bbd1be1dc273
unjoo <unjoo31@gmail.com>
2024-06-13 13:46:13 +0900

    setting

---
+++ src/main/java/shop/mtcoding/springtest_chatgptapi/SpringtestChatgptapiApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.springtest_chatgptapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestChatgptapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtestChatgptapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/springtest_chatgptapi/SpringtestChatgptapiApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.springtest_chatgptapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestChatgptapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
